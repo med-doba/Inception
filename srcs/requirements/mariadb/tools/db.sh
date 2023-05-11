48204177021f6824c018966046939bc0f5f62209
Mohammed Ed-dobaji <med-doba@e2r2p5.1337.ma>
2023-05-11 21:01:04 +0100

    fixed mariaDB

--- srcs/requirements/mariadb/tools/db.sh
+++ srcs/requirements/mariadb/tools/db.sh
@@ -1,12 +1,44 @@
 #!/bin/bash
 
-# Stop the MariaDB server
-service mysql stop
-
-# Initialize the MySQL data directory
-mysql_install_db --user=mysql --datadir=/var/lib/mysql
-CREATE USER 'root'@localhost IDENTIFIED BY 'mypassword';
-GRANT USAGE ON *.* TO 'root'@localhost IDENTIFIED BY 'mypassword';
+# # Initialize the MySQL data directory
+# mysql_install_db --datadir=/var/lib/mysql
 
 # Start the MariaDB server
+# mysqld --user=mysql --console &
 service mysql start
+
+# Wait for the server to start
+while ! mysqladmin ping -hlocalhost --silent; do
+	sleep 1
+done
+
+# sed -i 's/^skip-networking/#&/' /etc/mysql/mariadb.conf.d/50-server.cnf
+
+# Create the database
+mysql -e "CREATE DATABASE wordpress;"
+
+# create a new user with the username and password.
+# mysql -e "CREATE USER 'med'@'%' IDENTIFIED BY 'password';"
+mysql -e "SELECT User FROM mysql.user WHERE User='med';"
+mysql -e "DROP USER IF EXISTS 'med'@'%';"
+mysql -e "CREATE USER 'med'@'%' IDENTIFIED BY 'password';"
+
+
+# Grant all privileges to the user
+mysql -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'med'@'%';"
+
+# To apply the changes, flush the privileges.
+mysql -e "FLUSH PRIVILEGES;"
+
+# sleep 1000000
+sleep 5
+
+# # service mysql start
+
+# service mysql stop
+kill $(cat /var/run/mysqld/mysqld.pid)
+
+# service mysql status
+
+mysqld
+# exec $@
