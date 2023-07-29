7e331bbf7fd950355fbc91d4bef2426c49dbd2d4
Mohammed Ed-dobaji <med-doba@e3r3p18.1337.ma>
2023-07-29 04:27:36 +0100

    inception done

--- srcs/requirements/bonus/mailhog/tools/mailhog.sh
+++ srcs/requirements/bonus/mailhog/tools/mailhog.sh
@@ -3,14 +3,4 @@
 # Start MailHog
 MailHog
 
-# Send an email using curl command
-# curl --request POST \
-#      --url http://localhost:8025/ \
-#      --header 'Content-Type: application/json' \
-#      --data '{
-#        "To": ["mohaeddo03@gmail.com"],
-#        "From": "mohaeddo03@gmail.com",
-#        "Subject": "Test Email",
-#        "Text": "This is a test email."
-#      }'
 
